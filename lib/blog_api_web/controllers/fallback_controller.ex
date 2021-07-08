50a30d21806afb0870602394ae2fa6473e0c326b
Rodrigo <akio2631@gmail.com>
2021-07-08 00:39:20 +0000

    🥅 Add error handling on controllers

---
+++ lib/blog_api_web/controllers/fallback_controller.ex
@@ -0,0 +1,17 @@
+defmodule BlogApiWeb.FallbackController do
+  use BlogApiWeb, :controller
+
+  def call(conn, {:error, result, status_code}) when is_atom(status_code) or is_integer(status_code) do
+    conn
+    |> put_status(status_code)
+    |> put_view(ContaiServerWeb.ErrorView)
+    |> render("errors.json", result: result)
+  end
+
+  def call(conn, {:error, result}) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(ContaiServerWeb.ErrorView)
+    |> render("errors.json", result: result)
+  end
+end
--- lib/blog_api_web/views/error_view.ex
+++ lib/blog_api_web/views/error_view.ex
@@ -1,5 +1,20 @@
 defmodule BlogApiWeb.ErrorView do
   use BlogApiWeb, :view
+  alias Ecto.Changeset
+
+  def render("errors.json", %{result: %Changeset{} = changeset}),
+  do: %{message: translate_errors(changeset)}
+
+  def render("errors.json", %{result: message}),
+    do: %{message: message}
+
+  defp translate_errors(changeset) do
+    Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
 
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
