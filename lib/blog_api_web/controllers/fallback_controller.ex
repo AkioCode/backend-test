34b12834fdec5ffb666932bc7d37c9a4bf31f536
Rodrigo <akio2631@gmail.com>
2021-07-08 02:31:32 +0000

    ✨ Add error formatting

--- lib/blog_api_web/controllers/fallback_controller.ex
+++ lib/blog_api_web/controllers/fallback_controller.ex
@@ -1,17 +1,18 @@
 defmodule BlogApiWeb.FallbackController do
   use BlogApiWeb, :controller
 
-  def call(conn, {:error, result, status_code}) when is_atom(status_code) or is_integer(status_code) do
+  def call(conn, {:error, result, status_code})
+      when is_atom(status_code) or is_integer(status_code) do
     conn
     |> put_status(status_code)
-    |> put_view(ContaiServerWeb.ErrorView)
+    |> put_view(BlogApiWeb.ErrorView)
     |> render("errors.json", result: result)
   end
 
   def call(conn, {:error, result}) do
     conn
     |> put_status(:bad_request)
-    |> put_view(ContaiServerWeb.ErrorView)
+    |> put_view(BlogApiWeb.ErrorView)
     |> render("errors.json", result: result)
   end
 end
--- lib/blog_api_web/views/error_view.ex
+++ lib/blog_api_web/views/error_view.ex
@@ -2,8 +2,19 @@ defmodule BlogApiWeb.ErrorView do
   use BlogApiWeb, :view
   alias Ecto.Changeset
 
-  def render("errors.json", %{result: %Changeset{} = changeset}),
-  do: %{message: translate_errors(changeset)}
+  def render("errors.json", %{
+        result: %Changeset{
+          errors: [
+            email: {message, [constraint: :unique, constraint_name: "users_email_index"]}
+          ]
+        }
+      }) do
+    %{message: message}
+  end
+
+  def render("errors.json", %{result: %Changeset{} = changeset}) do
+    %{message: translate_errors(changeset)}
+  end
 
   def render("errors.json", %{result: message}),
     do: %{message: message}
@@ -13,6 +24,10 @@ defmodule BlogApiWeb.ErrorView do
       Enum.reduce(opts, msg, fn {key, value}, acc ->
         String.replace(acc, "%{#{key}}", to_string(value))
       end)
+    end)
+    |> Enum.reduce("", fn {k, v}, acc ->
+      joined_errors = Enum.join(v, "; ")
+      "#{acc}\"#{k}\" #{joined_errors}"
     end)
   end
 
