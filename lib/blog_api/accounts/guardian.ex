77ae3918d7dc5c0fdc126efea7ae1a95059fa6ba
Rodrigo <akio2631@gmail.com>
2021-07-08 03:31:16 +0000

    🔧 Setup guardian

--- lib/blog_api/accounts/guardian.ex
+++ lib/blog_api/accounts/guardian.ex
@@ -3,15 +3,26 @@ defmodule BlogApi.Accounts.Guardian do
 
   alias BlogApi.Accounts
 
-  def subject_for_token(user, _claims) do
-    {:ok, to_string(user.id)}
+  def subject_for_token(resource, _claims) do
+    sub = to_string(resource.id)
+    {:ok, sub}
   end
 
-  def resource_from_claims(claims) do
-    user =
-      claims["sub"]
-      |> Accounts.get_user!
-    {:ok, user}
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
   end
 
+  def resource_from_claims(%{"sub" => id}) do
+    resource = Accounts.get_user!(id)
+    {:ok, resource}
+  end
+
+  def resource_from_claims(_claims) do
+    {:error, :reason_for_error}
+  end
+
+  def sign_in(%Accounts.User{} = user),
+    do: Guardian.encode_and_sign(__MODULE__, user)
+
+  def verify(token), do: Guardian.decode_and_verify(__MODULE__, token)
 end
