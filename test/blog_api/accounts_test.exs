0668485da3dc5051e34969bfa7725fac88fd8754
Rodrigo Otsuka <akio2631@gmail.com>
2021-07-12 04:28:19 +0000

    ✅ Adjust tests to accounts

--- test/blog_api/accounts_test.exs
+++ test/blog_api/accounts_test.exs
@@ -8,13 +8,13 @@ defmodule BlogApi.AccountsTest do
 
     @valid_attrs %{
       displayName: "some displayName",
-      email: "some email",
+      email: "some@email",
       image: "some image",
       password: "some password"
     }
     @update_attrs %{
       displayName: "some updated displayName",
-      email: "some updated email",
+      email: "some updated@email",
       image: "some updated image",
       password: "some updated password"
     }
@@ -34,7 +34,7 @@ defmodule BlogApi.AccountsTest do
       assert Accounts.list_users() == [user]
     end
 
-    test "get_user!/1 returns the user with given id" do
+    test "get_user/1 returns the user with given id" do
       user = user_fixture()
       assert Accounts.get_user(user.id) == user
     end
@@ -42,7 +42,7 @@ defmodule BlogApi.AccountsTest do
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
       assert user.displayName == "some displayName"
-      assert user.email == "some email"
+      assert user.email == "some@email"
       assert user.image == "some image"
       assert user.password == "some password"
     end
@@ -55,7 +55,7 @@ defmodule BlogApi.AccountsTest do
       user = user_fixture()
       assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
       assert user.displayName == "some updated displayName"
-      assert user.email == "some updated email"
+      assert user.email == "some updated@email"
       assert user.image == "some updated image"
       assert user.password == "some updated password"
     end
@@ -69,12 +69,6 @@ defmodule BlogApi.AccountsTest do
     test "delete_user/1 deletes the user" do
       user = user_fixture()
       assert {:ok, %User{}} = Accounts.delete_user(user)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user(user.id) end
-    end
-
-    test "change_user/1 returns a user changeset" do
-      user = user_fixture()
-      assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
   end
 end
